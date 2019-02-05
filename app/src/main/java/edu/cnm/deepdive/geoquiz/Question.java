53bae113a6e2a31ab7cbfbd6cdc04cad4b8ff96b
Felipe Cook <felipepcook@gmail.com>
2019-02-05 07:41:06 -0700

    Added "Next" Button and image identifiers.

---
+++ app/src/main/java/edu/cnm/deepdive/geoquiz/Question.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive.geoquiz;
+
+public class Question {
+
+  private int mTextResID;
+  private boolean mAnswerTrue;
+
+  public int getTextResID() {
+    return mTextResID;
+  }
+
+  public void setTextResID(int textResID) {
+    mTextResID = textResID;
+  }
+
+  public boolean isAnswerTrue() {
+    return mAnswerTrue;
+  }
+
+  public void setAnswerTrue(boolean answerTrue) {
+    mAnswerTrue = answerTrue;
+  }
+
+  public Question(int textResID, boolean answerTrue) {
+    mTextResID = textResID;
+    mAnswerTrue = answerTrue;
+  }
+
+}
