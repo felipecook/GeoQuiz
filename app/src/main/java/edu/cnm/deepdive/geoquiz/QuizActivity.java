5593fb1f6e6075c90522636367bc7c3f67b8e8c9
Felipe Cook <felipepcook@gmail.com>
2019-02-12 20:20:56 -0700

    Added CheatActivity.class and Cheat button

--- app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
@@ -1,5 +1,8 @@
 package edu.cnm.deepdive.geoquiz;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +17,7 @@ public class QuizActivity extends AppCompatActivity {
   private Button mTrueButton;
   private Button mFalseButton;
   private Button mNextButton;
+  private Button mCheatButton;
   private TextView mQuestionTextView;
   private Question[] mQuestionBank = new Question[] {
       new Question(R.string.question_australia, true),
@@ -23,10 +27,13 @@ public class QuizActivity extends AppCompatActivity {
       new Question(R.string.question_americas, true),
       new Question(R.string.question_asia, true),
   };
+
   private int mCurrentIndex = 0;
+  private boolean mIsCheater;
 
   private static final String TAG = "QuizActivity";
   private static final String KEY_INDEX = "index";
+  private static final int REQUEST_CODE_CHEAT = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -61,12 +68,37 @@ public class QuizActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        mIsCheater = false;
         updateQuestion();
       }
 
     });
 
+    mCheatButton = (Button) findViewById(R.id.cheat_button);
+    mCheatButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+        startActivityForResult(intent, REQUEST_CODE_CHEAT);
+
+      }
+    });
     updateQuestion();
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    if (resultCode != Activity.RESULT_OK) {
+      return;
+    }
+
+    if (requestCode == REQUEST_CODE_CHEAT) {
+      if (data == null) {
+        return;
+      }
+      mIsCheater =CheatActivity.wasAnswerShown(data);
+    }
   }
 
   @Override
@@ -115,12 +147,16 @@ public class QuizActivity extends AppCompatActivity {
 
     int messageResId = 0;
 
-    if (userPressedTrue == answerIsTrue) {
-      messageResId = R.string.correct_toast;
+    if (mIsCheater) {
+      messageResId = R.string.judgement_toast;
     } else {
-      messageResId = R.string.incorrect_toast;
-    }
+      if (userPressedTrue == answerIsTrue) {
+        messageResId = R.string.correct_toast;
+      } else {
+        messageResId = R.string.incorrect_toast;
+      }
 
-    Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+      Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
   }
 }
